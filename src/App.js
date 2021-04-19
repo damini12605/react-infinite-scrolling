b2ba7f54d0fd678b4ecc0c338dac16f2f5e53466
Damini Sharma <dsharma@cognam.com>
2021-04-19 19:47:03 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useCallback } from 'react'
+import useBookSerach from './useBookSerach'
 
-function App() {
+export default function App() {
+  const [ query, setQuery ] = useState('')
+  const [pageNumber, setpageNumber] = useState(1)
+
+const { books, hasMore, loading, error } = useBookSerach(query, pageNumber)
+const observer = useRef()
+const lastBookElementRef = useCallback(node => {
+  if(loading) return
+  if (observer.current) observer.current.disconnect()
+  observer.current = new IntersectionObserver(entries => {
+   if(entries[0].isIntersecting && hasMore) {
+     setpageNumber(prevPageNumber => prevPageNumber + 1 )
+     console.log('Visible')
+   }
+  })
+  if (node) observer.current.observe(node)
+  console.log(node)
+}, [loading, hasMore])
+
+
+function handleSearch(e) {
+  setQuery(e.target.value)
+  setpageNumber(1)
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <input type="text" value={query} onChange={handleSearch}></input>
+   {books.map((book, index) => {
+     if(books.length === index + 1){
+      return <div ref={lastBookElementRef} key={book}>{book}</div>
+     } else{
+      return <div key={book}>{book}</div>
+     }
+     
+   })}
+   <div>{loading && 'Loading......'}</div>
+   <div>{error && 'Error'}</div>
+   </>
   );
 }
 
-export default App;
+//export default App;
